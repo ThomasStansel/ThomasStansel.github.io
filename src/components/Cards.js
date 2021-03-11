4cde9c487da9913e6a7244f426dd3a57afbd8109
Thomas Stansel <thomas.s.stansel@gmail.com>
2021-03-10 17:24:35 -0700

    Added AlexaCAPS Project -> won't push to live version until AlexaCAP's bugs are fleshed out

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,8 +8,19 @@ function Cards() {
             <h1 id="projects">Projects</h1>
             <div className="cards-container">
                 <div className="cards-wrapper">
-                    <ul className="cards-items">
+                <ul className="cards-items">
                         <CardItem
+                            icon="fab fa-python"
+                            text="AlexaCAPS"
+                            label="Alexa"
+                            description="An Alexa Skill that is triggered when the student asks for their university's
+                            Counseling and Psychological Services(CAPS) phone number. A lambda function then uses web scraping to get the specfied university's
+                            CAPS number and gives it back to the student through the Alexa device."
+                            github="https://github.com/TStansel/HackUNO"
+                            />
+                    </ul>
+                <ul className="cards-items">
+                    <CardItem
                             icon="fab fa-android"
                             text="Family Map Client"
                             label="Android"
@@ -17,7 +28,7 @@ function Cards() {
                             markers for each of the user's and their family's events are displayed on a map.
                             Filters were created to allow the user to filter out certain events from being shown on the map."
                             />
-                        <CardItem
+                    <CardItem
                             icon="fab fa-java"
                             text="Family Map Server"
                             label="Java"
@@ -25,7 +36,7 @@ function Cards() {
                                 This project was responsible for generating four generations of data for the user and storing it in a SQL server.
                                 This project also created an API that allowed the Android app to access this data.'
                             />
-                        <CardItem
+                    <CardItem
                             icon="fab fa-python"
                             text="TherAssist"
                             label="Python"
--- src/components/MtnCards.js
+++ src/components/MtnCards.js
@@ -13,24 +13,25 @@ function MtnCards() {
     return (
         <div className="mtn-cards">
             <h1 className="mtn-header" id="mountains">National Parks</h1>
+            <h3 className="mtn-desc" id="mtn-desc">I have a goal to visit all the National Parks in the US these are the ones I've gone to so far!</h3>
             <div className="mtn-cards-container">
                 <div className="mtn-cards-wrapper">
                     <ul className="mtn-cards-items">
                         <MtnCardItem
                             src={zions}
-                            text="Zions Natl Park"
+                            text="Zions"
                             label="Angels Landing"
                             description="End of Angels Landing Hike in Febuary 2021"
                             />
                         <MtnCardItem
                             src={basin}
-                            text="Great Basin Natl Park"
+                            text="Great Basin"
                             label="Great Basin"
                             description='Summit of Wheeler Peak in September 2020'
                             />
                         <MtnCardItem
                             src={tetons}
-                            text="Grand Teton Natl Park"
+                            text="Grand Tetons"
                             label="Grand Tetons"
                             description="
                                 Picture of the Grand Tetons in August 2020"
@@ -39,19 +40,19 @@ function MtnCards() {
                     <ul className="mtn-cards-items">
                         <MtnCardItem
                             src={yellowstone}
-                            text="Yellowstone Natl Park"
+                            text="Yellowstone"
                             label="Ol' Faithful"
                             description="Grand Canyon of Yellowstone in August 2020"
                             />
                         <MtnCardItem
                             src={capital_reef}
-                            text="Capital Reef Natl Park"
+                            text="Capital Reef"
                             label="Capital Reef"
                             description='Small Outlook over Capital Reef in May 2020'
                             />
                         <MtnCardItem
                             src={guadalupe}
-                            text="Guadalupe Mountains Natl Park"
+                            text="Guadalupe Mountains"
                             label="Guadalupe Moutains"
                             description="
                                 Summit of Guadalupe Peak in August 2016"
